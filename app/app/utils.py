8dd519d481bf3c904d1e4cccf0b0044072086677
ajpl <ajpl@users.noreply.github.com>
2020-11-26 02:13:39 -0300

    Add black and flake8

--- app/app/utils.py
+++ app/app/utils.py
@@ -11,15 +11,15 @@ from PIL import (
 def generate_image(caller_id):
     img = Image.open("assets/base.png")
     font = ImageFont.truetype("assets/arial-black.ttf", font_size(caller_id))
-    text = Image.new('L', (428, 349))
+    text = Image.new("L", (428, 349))
     draw = ImageDraw.Draw(text)
     draw.text((10, 105), caller_id, font=font, fill=255)
     img_to_paste = text.rotate(-10, expand=1)
     img.paste("#FFF", (0, 0), img_to_paste)
     img_io = BytesIO()
-    img.save(img_io, 'PNG')
+    img.save(img_io, "PNG")
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/png', cache_timeout=0)
+    return send_file(img_io, mimetype="image/png", cache_timeout=0)
 
 
 def font_size(caller_id):
